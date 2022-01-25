d32d2deff8ba3ddb0f5ba418da836c9ebdb4e1ad
yusufnasir09 <67826184+yusufnasir09@users.noreply.github.com>
2022-01-25 19:58:01 +0100

    test

--- App.js
+++ App.js
@@ -3,18 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Oto start working Syou</Text>
+    <View style={{flex: 1, padding: 20}}>
+    <View>
+      <Text>Oto start working</Text>
       <StatusBar style="auto" />
+    </View>
+    <View style={{flex: 1, flexGrow: 1,backgroundColor: 'blue'}}>
+<Text>Body</Text>
+    </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
