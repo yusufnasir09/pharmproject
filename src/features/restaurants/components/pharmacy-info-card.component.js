827bfa4b37c2cf78ef3f4b2ae31ba57c298c508b
yusufnasir09 <67826184+yusufnasir09@users.noreply.github.com>
2022-01-29 21:52:12 +0100

    day two

--- src/features/restaurants/components/pharmacy-info-card.component.js
+++ src/features/restaurants/components/pharmacy-info-card.component.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { StyleSheet, Text, Image, View } from 'react-native';
 import styled from 'styled-components/native'
 import { SvgXml } from 'react-native-svg';
-import { star, open } from "../../../../assets/star";
-import { StyleSheet, Text, View } from 'react-native';
+import { Spacer } from "../../../components/spacer/spacer.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
 import { Card } from 'react-native-paper';
 
 const PharmacyCard = styled(Card)`
@@ -13,16 +16,18 @@ padding: ${(props) => props.theme.space[3]};
 background-color: ${(props) => props.theme.colors.bg.secondary};
 `;
 const Section = styled(View)`
+flex-direction: row;
+align-items: center;
+`;
+const Rating = styled(View)`
 padding-top: ${(props) => props.theme.space[2]};
 padding-bottom: ${(props) => props.theme.space[2]};
 flex-direction: row;
 `;
-
-const Rating = styled(View)`
-justify-content: flex-start;
-`;
-const Open = styled(View)`
+const SectionEnd = styled(View)`
+flex: 1;
 justify-content: flex-end;
+flex-direction: row;
 `;
 const Info = styled(View)`
 padding: ${(props) => props.theme.space[3]};
@@ -40,14 +45,14 @@ color: ${(props) => props.theme.colors.ui.primary};
 export const PharmacyInfoCard = ({ pharmacy = {} }) => {
     const {
         name = 'Some Pharmacy',
-        icon,
+        icon = "https://maps.gstatic.com/mapfiles/ms2/micons/hospitals.png",
         photos = [
             "https://www.pharmaccess.org/wp-content/uploads/2017/09/Julie-Harrison-Pharmacy-Day2017-8206.jpg",
         ],
         address = "20 streets nearby",
         isOpenNow = true,
         rating = 4,
-        isClosedTemporarily,
+        isClosedTemporarily = true,
     } = pharmacy;
     const ratingArray = Array.from(new Array(Math.floor(rating)));
     return (
@@ -57,13 +62,22 @@ export const PharmacyInfoCard = ({ pharmacy = {} }) => {
                 <Title>{name}</Title>
                 <Section>
                     <Rating>
-                        {ratingArray.map(() => <SvgXml xml={star} width={20} height={20} />)}
+                        {ratingArray.map(() => (
+                            <SvgXml xml={star} width={20} height={20} />
+                        ))}
                     </Rating>
-                    <Open>
-                        {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-                    </Open>
+                    <SectionEnd>
+                        {isClosedTemporarily && (
+                            <Text variant="label" style={{ color: 'red', }}>CLOSED TEMORARILY</Text>
+                        )}
+                        <Spacer position="left" size="medium">
+                            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+                        </Spacer>
+                        <Spacer position="left" size="medium">
+                            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+                        </Spacer>
+                    </SectionEnd>
                 </Section>
-
                 <Address>{address}</Address>
             </Info>
         </PharmacyCard>
